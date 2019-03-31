42432961c820bc90555fa39c6991c7c76d18470b
opiopan <opiopan@gmail.com>
2019-03-31 21:44:26 +0900

    add panelizing script

---
+++ pcb/panelize/merge.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+import gerberex
+from gerberex import DxfFile, GerberComposition, DrillComposition
+
+exts = ['GTL', 'GTO', 'GTP', 'GTS', 'GBL', 'GBO', 'GBP', 'GBS', 'TXT']
+boards=[
+    ('../small/CAMOutputs/rcstick-f-small.', 0, 60, 0),
+    ('../small/CAMOutputs/rcstick-f-small.', 20, 60, 0),
+    ('../small/CAMOutputs/rcstick-f-small.', 40, 60, 0),
+    ('../small/CAMOutputs/rcstick-f-small.', 60, 60, 0),
+    ('../small/CAMOutputs/rcstick-f-small.', 80, 60, 0),
+    ('../small/CAMOutputs/rcstick-f-small.', 15.4, 45, 180),
+    ('../small/CAMOutputs/rcstick-f-small.', 35.4, 45, 180),
+    ('../small/CAMOutputs/rcstick-f-small.', 55.4, 45, 180),
+    ('../small/CAMOutputs/rcstick-f-small.', 75.4, 45, 180),
+    ('../small/CAMOutputs/rcstick-f-small.', 95.4, 45, 180),
+]
+outline = 'outline.dxf'
+mousebites = 'mousebites.dxf'
+fill = 'fill.dxf'
+outputs = 'outputs/rcstick-panelized'
+
+for ext in exts:
+    print('merging %s: ' % ext ,end='', flush=True)
+    if ext == 'TXT':
+        ctx = DrillComposition()
+    else:
+        ctx = GerberComposition()
+    for board in boards:
+        file = gerberex.read(board[0] + ext)
+        file.to_metric()
+        file.rotate(board[3])
+        file.offset(board[1], board[2])
+        ctx.merge(file)
+        print('.', end='', flush=True)
+    if ext == 'TXT':
+        file = gerberex.read(mousebites)
+        file.draw_mode = DxfFile.DM_MOUSE_BITES
+        file.width = 0.5
+        file.format = (3, 3)
+        ctx.merge(file)
+    else:
+        file = gerberex.read(outline)
+        ctx.merge(file)
+    ctx.dump(outputs + '.' + ext)
+    print(' end', flush=True)
+
+print('generating GML: ', end='', flush=True)
+file = gerberex.read(outline)
+file.write(outputs + '.GML')
+print('.', end='', flush=True)
+ctx = GerberComposition()
+file = gerberex.read(fill)
+file.to_metric()
+file.draw_mode = DxfFile.DM_FILL
+ctx.merge(file)
+ctx.dump(outputs + '-fill.GML')
+
+print('. end', flush=True)
